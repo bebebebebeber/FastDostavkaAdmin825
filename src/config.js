669056cd12c97a1c7941f9189fcd189c47b11e8c
Daniil <danik22122005@gmail.com>
2020-10-22 13:43:43 +0300

    maks is bad

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-//export const serverUrl = "https://fastdostavka.ga/";
+export const serverUrl = "https://fastdostavka.ga/";
 //public static final String BASE_URL = "https://fastdostavka.ga";
 //public static final String BASE_URL = "http://10.0.2.2";
-export const serverUrl = "http://127.0.0.1/";
+//export const serverUrl = "http://127.0.0.1/";
